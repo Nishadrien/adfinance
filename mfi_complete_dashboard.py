45324b5897d2f5df96ea15d9f17e47e7bdd6c6f8
Nishimirwe Adrien <88964932+Nishadrien@users.noreply.github.com>
2025-08-15 17:06:16 +0200

    Update mfi_complete_dashboard.py

--- mfi_complete_dashboard.py
+++ mfi_complete_dashboard.py
@@ -126,13 +126,6 @@ st.markdown("""
     .block-container {
         padding-top: 0px;
     }
-    .stApp {
-        background-image: url("download.png");
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-        background-position: center;
-    }
     .stApp > div > div {
         background-color: rgba(255, 255, 255, 0.8);
         padding: 20px;
@@ -472,3 +465,4 @@ if st.button('Submit'):
             st.page_link("https://cenfriglobal.sharepoint.com/:x:/r/sites/ClientMastercardFoundation/Shared%20Documents/2.%20REDP2/08.%20Data%20Hub/MCFD2412%20-%20AD%20Finance%20analytics/Analytical%20framework/Final%20Version/AD%20Finance%20Analytical%20framework_with%20tables_columns.xlsb.xlsx?d=w23c7e5eeb11a4cf891be379f35562294&csf=1&web=1&e=zZ2yvq", label="Check Details", icon="🗒️")
 
                     
+

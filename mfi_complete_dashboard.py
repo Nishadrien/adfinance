1150186dfd96e375085dbda627205b872a32f094
Nishimirwe Adrien <88964932+Nishadrien@users.noreply.github.com>
2025-08-18 13:08:15 +0200

    Update mfi_complete_dashboard.py

--- mfi_complete_dashboard.py
+++ mfi_complete_dashboard.py
@@ -499,32 +499,7 @@ if st.button('Submit'):
             </div>
             """, unsafe_allow_html=True)
 
-            # Display table below the legend
-            table_html = """
-            <table class="general-scores-table">
-                <tr>
-                    <th>MFI</th>
-                    <th>Completeness</th>
-                    <th>Conformity</th>
-                    <th>Uniqueness</th>
-                    <th>Validity</th>
-                    <th>Integrity</th>
-                </tr>
-            """
-            for mfi, scores in general_scores.items():
-                table_html += "<tr>"
-                table_html += f"<td>{mfi}</td>"
-                for score in scores:
-                    if score >= 85:
-                        css_class = "score-good"
-                    elif score >= 75:
-                        css_class = "score-warning"
-                    else:
-                        css_class = "score-bad"
-                    table_html += f"<td class='{css_class}'>{score}%</td>"
-                table_html += "</tr>"
-            table_html += "</table>"
-            st.markdown(table_html, unsafe_allow_html=True)
+            
 
             with st.container(border=True):
                 right, middle = st.columns(2)
@@ -541,3 +516,4 @@ if st.button('Submit'):
                     st.subheader('Used Columns')
                     st.markdown(used_columns_markdown)
             st.page_link("https://cenfriglobal.sharepoint.com/:x:/r/sites/ClientMastercardFoundation/Shared%20Documents/2.%20REDP2/08.%20Data%20Hub/MCFD2412%20-%20AD%20Finance%20analytics/Analytical%20framework/Final%20Version/AD%20Finance%20Analytical%20framework_with%20tables_columns.xlsb.xlsx?d=w23c7e5eeb11a4cf891be379f35562294&csf=1&web=1&e=zZ2yvq", label="Check Details", icon="🗒️")
+
